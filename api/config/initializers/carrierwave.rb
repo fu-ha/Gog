3cb37f2e664e9a1d5c30ffa71ff4e59866284353
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2022-10-27 06:37:18 +0000

    'dotenv-rails'でENV['xxx']の形でrailsアプリ内で環境変数が使える。(database.ymlでは<%= ENV['xxx'] %と書く)。carrierwave.rbにもENV用いた。next.config.jsにImage使うためにファイルの保存先のドメインを記述。

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
   
   else
-    #config.asset_host = ENV['CARRIERWAVE_HOST'] 
-    config.asset_host = 'https://e5eb-18-183-169-161.ngrok.io'
+    config.asset_host = ENV['CARRIERWAVE_HOST'] 
+    #config.asset_host = 'https://ed82-54-168-213-6.ngrok.io'
     config.storage = :file
     config.cache_storage = :file
   end
--- front/next.config.js
+++ front/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['e5eb-18-183-169-161.ngrok.io'],
-    //domains: [process.env.NEXT_IMAGE_DOMAIN],
+    //domains: ['e5eb-18-183-169-161.ngrok.io'],
+    domains: [process.env.NEXT_IMAGE_DOMAIN],
   },
 }
 
