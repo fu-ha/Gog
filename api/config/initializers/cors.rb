f3f58c9d75fb29d4f0de4075277dfe7e5aa08669
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-03-09 09:37:44 +0000

    cors origins 'geem84.com' => 'https://geem84.com'

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # origins 'localhost:8081', '6a230d9e32254bf18f34da11905ca812.vfs.cloud9.ap-northeast-1.amazonaws.com:8081'
     # origins 'https://geem84.com', 'https://localhost:8081'
     # origins ENV['CORS_ORIGIN'] || ''
-    origins 'geem84.com'
+    origins 'https://geem84.com'
     
     resource '*',
       headers: :any,
