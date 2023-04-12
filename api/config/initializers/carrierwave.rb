d32d618145307ae41d2c1a16b0bb7014519bfaef
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-04-12 19:04:56 +0000

    carrierwave ENV~

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
   
   else
-    # config.asset_host = ENV['CARRIERWAVE_HOST'] 
-    config.asset_host = 'https://geem84.work'
+    config.asset_host = ENV['CARRIERWAVE_HOST'] 
+    # config.asset_host = 'https://geem84.work'
     config.storage = :file
     config.cache_storage = :file
   end
