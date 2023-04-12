3fd331430e624f88fe6689016d09d7df97bda7b7
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-04-12 22:35:39 +0000

    carrierwave cors ENVコメントアウト

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://geem84.com'
-    origins ENV['CORS_ORIGINS'] || ''
+    origins 'https://geem84.com'
+    # origins ENV['CORS_ORIGINS'] || ''
     
     resource '*',
       headers: :any,
