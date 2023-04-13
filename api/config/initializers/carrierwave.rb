3e99a14f7704dc5dff4da1835663d1dbbdefa43d
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-04-13 00:49:53 +0000

    ser_static_assets=rue

--- api/config/initializers/carrierwave.rb
+++ api/config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   # if Rails.env.production?
-    config.asset_host = 'https://geem84.work/api/v1'
+    config.asset_host = 'https://geem84.work'
   # else
     # config.asset_host = ENV['CARRIERWAVE_HOST'] 
     config.storage = :file
