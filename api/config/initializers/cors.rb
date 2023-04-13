a4ea98ed0a644e9b61c3667b45ee587cfcefce9f
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-04-13 20:54:40 +0000

    cors origins

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'] || ''
+    # origins ENV['CORS_ORIGINS'] || ''
+    origins 'https://geem84.com'
     
     resource '*',
       headers: :any,
