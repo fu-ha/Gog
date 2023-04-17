a76a08bdd752194ccc8ae2b61a825a611e2d5a56
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-04-17 05:52:40 +0000

    gog-rds recreate

--- front/next.config.js
+++ front/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
       // domains: [process.env.NEXT_IMAGE_DOMAIN],
       domains: ['geem84.work'],
     },
-    staticPageGenerationTimeout: 500,
+    // staticPageGenerationTimeout: 500,
     env: {
       NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     },
