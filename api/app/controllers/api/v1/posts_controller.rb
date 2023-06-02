0713d7530f57436e75308001b2d924d42fe3edf6
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-06-02 19:50:03 +0000

    posts_controller.rb コメント削除で修正

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -32,14 +32,10 @@ class Api::V1::PostsController < ApplicationController
       created_at: post.created_at,
       tag: post.tag,
       post_liked_count: PostLike.where(post_id: post.id).count,
-      #post_liked: PostLike.where(user_id: post.user_id, post_id: post.id).exists?
       post_liked: PostLike.where(user_id: current_api_v1_user.id, post_id: post.id).exists?,
-      #liked_icon: post.liked
-      #liked_count: PostLike.where(post_id: post.id, post_liked: true).count,
       post_like: PostLike.find_by(post_id: post.id),
       comment: Comment.find_by(post_id: post.id),
       comment_count: Comment.where(post_id: post.id).count,
-      #comment_like: CommentLike.find_by(post_id: post.id),
       comment_liked: CommentLike.where(user_id: current_api_v1_user.id, post_id: post.id).exists?,
       comment_liked_count: CommentLike.where(post_id: post.id).count
     }
