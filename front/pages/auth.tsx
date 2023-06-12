ace8ea7d0e3446063ed3c1c74b364395e6d6ee08
EC2 Default User <ec2-user@ip-10-0-0-219.ap-northeast-1.compute.internal>
2023-06-12 08:11:36 +0000

    /auth 新規登録モーダルの背景色の統一

--- front/pages/auth.tsx
+++ front/pages/auth.tsx
@@ -40,7 +40,7 @@ const Auth = () => {
   return(
     <div className="h-screen flex flex-col overflow-hidden md:overflow-hidden dark:bg-gray-900 dark:border-gray-600">
       <div className="flex flex-col bg-bgPrimary dark:bg-dark-900 relative items-center md:grow">
-        <header className="bg-white shadow sm:sticky sm:top-0 z-30 overscroll-none w-full flex-shrink-0 dark:bg-gray-900 border-b dark:border-gray-700">
+        <header className="bg-white shadow sm:sticky sm:top-0 z-10 overscroll-none w-full flex-shrink-0 dark:bg-gray-900 border-b dark:border-gray-700">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 dark:bg-gray-900 dark:border-gray-800">
             <div className="flex justify-between h-16">
               <div className="flex">
